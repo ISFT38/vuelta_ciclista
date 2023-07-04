d20a7bd975604d329d7a2510bd5993307352b66a
Victor Declerk <vdeclerk@itpatagonia.com>
2023-07-04 19:09:29 -0300

    Se terminan de agregar los datos

--- vuelta_ciclista.sql
+++ vuelta_ciclista.sql
@@ -5,45 +5,45 @@
 BEGIN;
 
 CREATE TABLE nacionalidades (
-	nacionalidad_id  INTEGER PRIMARY KEY,
-	nacionalidad     VARCHAR(128)
+    nacionalidad_id  INTEGER PRIMARY KEY,
+    nacionalidad     VARCHAR(128)
 );
 
 CREATE TABLE ciclistas (
-	ciclista_id		SERIAL PRIMARY KEY,
-	nombre			VARCHAR(256),
-	nacionalidad_id INTEGER REFERENCES nacionalidades,
-	f_nacimiento    DATE
+    ciclista_id     SERIAL PRIMARY KEY,
+    nombre          VARCHAR(256),
+    nacionalidad_id INTEGER REFERENCES nacionalidades,
+    f_nacimiento    DATE
 );
 
 CREATE TABLE equipos (
-	equipo_id		SERIAL PRIMARY KEY,
-	nombre          VARCHAR(256),
-	nacionalidad_id INTEGER REFERENCES nacionalidades,
-	director        VARCHAR(256)
+    equipo_id       SERIAL PRIMARY KEY,
+    nombre          VARCHAR(256),
+    nacionalidad_id INTEGER REFERENCES nacionalidades,
+    director        VARCHAR(256)
 );
 
 CREATE TABLE pruebas (
-	prueba_id		SERIAL PRIMARY KEY,
-	nombre			VARCHAR(256) UNIQUE,
-	edicion         INTEGER,
-	etapas			INTEGER,
-	kilometros		REAL,
-	ganador         INTEGER REFERENCES ciclistas(ciclista_id)
+    prueba_id   SERIAL PRIMARY KEY,
+    nombre      VARCHAR(256) UNIQUE,
+    edicion     INTEGER,
+    etapas      INTEGER,
+    kilometros  REAL,
+    ganador     INTEGER REFERENCES ciclistas(ciclista_id)
 );
 
 CREATE TABLE contratos (
-	ciclista_id		INTEGER REFERENCES ciclistas,
-	equipo_id		INTEGER REFERENCES equipos,
-	f_inicio		DATE NOT NULL,
-	f_fin			DATE
+    ciclista_id   INTEGER REFERENCES ciclistas,
+    equipo_id     INTEGER REFERENCES equipos,
+    f_inicio      DATE NOT NULL,
+    f_fin         DATE
 );
 
 CREATE TABLE equipos_pruebas (
-	equipo_id		INTEGER REFERENCES equipos,
-	prueba_id		INTEGER REFERENCES pruebas,
-	puesto			INTEGER,
-	UNIQUE (equipo_id, prueba_id)
+    equipo_id   INTEGER REFERENCES equipos,
+    prueba_id   INTEGER REFERENCES pruebas,
+    puesto      INTEGER,
+    UNIQUE (equipo_id, prueba_id)
 );
 
 COMMIT;
--- vuelta_ciclista_datos.sql
+++ vuelta_ciclista_datos.sql
@@ -4,10 +4,35 @@ INSERT INTO nacionalidades(nacionalidad_id, nacionalidad) VALUES
 	(1, 'Argentina'),
 	(2, 'Uruguaya');
 
-INSERT INTO ciclistas(nombre, nacionalidad)
-DROP TABLE equipos_pruebas;
-DROP TABLE contratos;
-DROP TABLE pruebas;
-DROP TABLE equipos;
-DROP TABLE ciclistas;
+INSERT INTO ciclistas(nombre, nacionalidad_id) VALUES
+	('Juan Perez', 1),
+	('Maria Gomez', 2);
 
+INSERT INTO equipos(nombre, nacionalidad_id, director) VALUES
+	('Rayo', 1, 'Mc Queen'),
+	('Nube', 2, 'Mc Goo');
+
+INSERT INTO pruebas(nombre, edicion, etapas, kilometros) VALUES
+  ('Vuelta de Cordoba', 2023, 4, 320);
+
+INSERT INTO contratos(ciclista_id, equipo_id, f_inicio) VALUES
+  (1, 1, '2021-05-10'),
+  (2, 2, '2022-02-22');
+  
+ INSERT INTO equipos_pruebas(equipo_id, prueba_id, puesto) VALUES
+  (2, 1, 1),
+  (1, 1, 2);
+
+UPDATE pruebas SET ganador = (SELECT ciclista_id FROM ciclistas WHERE nombre = 'Maria Gomez')
+  WHERE prueba_id = 1;
+
+-- UPDATE pruebas SET ganador = 5
+--   WHERE prueba_id = 1;
+  
+-- DELETE FROM ciclistas WHERE ciclista_id = 2;
+
+-- DROP TABLE equipos_pruebas;
+-- DROP TABLE contratos;
+-- DROP TABLE pruebas;
+-- DROP TABLE equipos;
+-- DROP TABLE ciclistas;
